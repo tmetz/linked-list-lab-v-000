af3bfafa63537c29740b84e96e77fa0690ede880
Tammy Metz <tammy@tammymetz.com>
2019-03-17 00:16:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,11 @@ function nodeAt(index, linkedList, collection) {
 }
 
 function addressAt(index, linkedList, collection) {
-  return (collection[nodeAt(index, linkedList, collection)]);
+  if (index == 0) {
+    return linkedList;
+  }
+  else {
+    let node = nodeAt(index - 1, linkedList, collection);
+    return node.next;
+  }
 }
